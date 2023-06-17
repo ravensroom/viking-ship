d74a775652bc2b29b055f11e64bf3c6d56521be7
raven <raven.lin.010@gmail.com>
2023-06-16 22:31:53 -0700

    SubMenu test cases

--- src/components/Menu/menu.test.tsx
+++ src/components/Menu/menu.test.tsx
@@ -4,9 +4,11 @@ import {
   render,
   RenderResult,
   cleanup,
+  act,
 } from '@testing-library/react';
 import Menu, { MenuProps } from './menu';
 import MenuItem from './menuItem';
+import SubMenu from './subMenu';
 
 const testProps: MenuProps = {
   defaultIndex: '0',
@@ -25,9 +27,26 @@ const generalMenu = (props: MenuProps) => {
       <MenuItem>active</MenuItem>
       <MenuItem disabled>disabled</MenuItem>
       <MenuItem>third</MenuItem>
-      <MenuItem>Cool Link</MenuItem>
+      <SubMenu title="dropdown">
+        <MenuItem>drop1</MenuItem>
+      </SubMenu>
     </Menu>
   );
+};
+
+const createStyleFile = () => {
+  const cssFile: string = `
+    .viking-submenu {
+      display:none;
+    }
+    .viking-submenu.menu-opened {
+      display:block;
+    }
+  `;
+
+  const style = document.createElement('style');
+  style.innerHTML = cssFile;
+  return style;
 };
 
 let wrapper: RenderResult,
@@ -38,14 +57,17 @@ let wrapper: RenderResult,
 describe('test Menu and MenuItem component', () => {
   beforeEach(() => {
     wrapper = render(generalMenu(testProps));
+    wrapper.container.append(createStyleFile());
     menuElement = wrapper.getByTestId('test-menu');
     activeElement = wrapper.getByText('active');
     disabledElement = wrapper.getByText('disabled');
+
+    jest.useFakeTimers();
   });
   it('should render correct Menu and MenuItem based on default props', () => {
     expect(menuElement).toBeInTheDocument();
     expect(menuElement).toHaveClass('viking-menu test');
-    expect(menuElement.getElementsByTagName('li').length).toEqual(4);
+    expect(menuElement.querySelectorAll(':scope > li').length).toEqual(4);
     expect(activeElement).toHaveClass('menu-item is-active');
     expect(disabledElement).toHaveClass('menu-item is-disabled');
   });
@@ -54,15 +76,26 @@ describe('test Menu and MenuItem component', () => {
     fireEvent.click(thirdItem);
     expect(thirdItem).toHaveClass('is-active');
     expect(activeElement).not.toHaveClass('is-active');
-    expect(testProps.onSelect).toHaveBeenCalledWith(2);
+    expect(testProps.onSelect).toHaveBeenCalledWith('2');
     fireEvent.click(disabledElement);
     expect(disabledElement).not.toHaveClass('is-active');
-    expect(testProps.onSelect).not.toHaveBeenCalledWith(1);
+    expect(testProps.onSelect).not.toHaveBeenCalledWith('1');
   });
   it('should render vertical mode when set so', () => {
     cleanup();
     wrapper = render(generalMenu(testVerticalProps));
     menuElement = wrapper.getByTestId('test-menu');
     expect(menuElement).toHaveClass('menu-vertical');
+  });
+  it('should show dropdown items when hover on subMenu', async () => {
+    // const dpItem = wrapper.queryByText('drop1');
+    // expect(dpItem).not.toBeVisible();
+    // const dpMenu = wrapper.getByText('dropdown');
+    // fireEvent.mouseEnter(dpMenu);
+    // act(() => jest.runAllTimers());
+    // expect(dpItem).toBeVisible();
+    // fireEvent.mouseLeave(dpMenu);
+    // act(() => jest.runAllTimers());
+    // expect(dpItem).not.toBeVisible();
   });
 });
